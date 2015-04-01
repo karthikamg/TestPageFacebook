627914e897b040d9271eb3178f8b2e4bcdd9cdf1
karthika <karthikam@qburst.com>
2015-04-01 09:32:03 +0530

    Cover Page added

---
+++ images/fbpage_files/N_wbWmx3_kc.js
@@ -0,0 +1,5 @@
+/*!CK:2424910399!*//*1427086733,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["Xvyeu"]); }
+
+__d("XOfferSendPlatformNotificationController",["XController"],function(a,b,c,d,e,f){b.__markCompiled&&b.__markCompiled();e.exports=b("XController").create("\/offers\/v2\/send_platform_notification\/",{});},null);
