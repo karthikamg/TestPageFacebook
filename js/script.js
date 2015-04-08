b0f745f4bee8c6c96fd872d0dd41771b57d774c7
karthika <karthikam@qburst.com>
2015-04-08 08:29:46 +0530

    Status 2

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function aa(){
+	var tt=document.getElementById('postsite-size');
+	tt.style.display='block';
+}
+document.getElementById("status-expand").onclick = function() {myFunction()};
+
+function myFunction() {
+    document.getElementById("postsite-size").innerHTML = "YOU CLICKED ME!";}
